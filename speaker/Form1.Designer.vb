99499ae741f7e4357891ab48e3c4f1ea46c4558a
صديق عبد الفتاح <  >
2017-03-28 11:06:10 +0100

    original source code

---
+++ speaker/Form1.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FacebookToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PlusToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.YoutubeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.PictureBox1.Image = Global.speaker.My.Resources.Resources.Very_Basic_Close_icon
+        Me.PictureBox1.Location = New System.Drawing.Point(305, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(33, 30)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.PictureBox2.Image = Global.speaker.My.Resources.Resources.minimize_icon_23
+        Me.PictureBox2.Location = New System.Drawing.Point(263, 12)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(36, 30)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackColor = System.Drawing.SystemColors.Control
+        Me.PictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox3.Image = Global.speaker.My.Resources.Resources.post_3952_0_19556900_1417208945
+        Me.PictureBox3.Location = New System.Drawing.Point(12, 49)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(326, 230)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 2
+        Me.PictureBox3.TabStop = False
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(350, 24)
+        Me.MenuStrip1.TabIndex = 3
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FacebookToolStripMenuItem, Me.PlusToolStripMenuItem, Me.YoutubeToolStripMenuItem})
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(52, 20)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'FacebookToolStripMenuItem
+        '
+        Me.FacebookToolStripMenuItem.Name = "FacebookToolStripMenuItem"
+        Me.FacebookToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.FacebookToolStripMenuItem.Text = "facebook"
+        '
+        'PlusToolStripMenuItem
+        '
+        Me.PlusToolStripMenuItem.Name = "PlusToolStripMenuItem"
+        Me.PlusToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.PlusToolStripMenuItem.Text = "plus"
+        '
+        'YoutubeToolStripMenuItem
+        '
+        Me.YoutubeToolStripMenuItem.Name = "YoutubeToolStripMenuItem"
+        Me.YoutubeToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.YoutubeToolStripMenuItem.Text = "youtube"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(350, 291)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "voice"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox3 As System.Windows.Forms.PictureBox
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents AboutToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents FacebookToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PlusToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents YoutubeToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ speaker/Form1.vb
@@ -0,0 +1,92 @@
+﻿Imports System.Speech
+
+Public Class Form1
+
+    Dim WithEvents reco As New Recognition.SpeechRecognitionEngine
+
+    Private Sub SetColor(ByVal color As System.Drawing.Color)
+
+        Dim synth As New Synthesis.SpeechSynthesizer
+
+        synth.SpeakAsync("setting the back color to " + color.ToString)
+
+        Me.BackColor = color
+
+    End Sub
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        reco.SetInputToDefaultAudioDevice()
+
+        Dim gram As New Recognition.SrgsGrammar.SrgsDocument
+
+        Dim colorRule As New Recognition.SrgsGrammar.SrgsRule("color")
+
+        Dim colorsList As New Recognition.SrgsGrammar.SrgsOneOf("facebook", "youtube", "google")
+
+        colorRule.Add(colorsList)
+
+        gram.Rules.Add(colorRule)
+
+        gram.Root = colorRule
+
+        reco.LoadGrammar(New Recognition.Grammar(gram))
+
+        reco.RecognizeAsync()
+
+    End Sub
+
+    Private Sub reco_RecognizeCompleted(ByVal sender As Object, ByVal e As System.Speech.Recognition.RecognizeCompletedEventArgs) Handles reco.RecognizeCompleted
+
+        reco.RecognizeAsync()
+
+    End Sub
+
+    Private Sub reco_SpeechRecognized(ByVal sender As Object, ByVal e As System.Speech.Recognition.RecognitionEventArgs) Handles reco.SpeechRecognized
+
+        Select Case e.Result.Text
+
+            Case "facebook"
+
+                Dim url As String = "https://www.facebook.com"
+                Process.Start(url)
+
+            Case "youtube"
+
+                Dim url As String = "https://www.youtube.com/"
+                Process.Start(url)
+
+            Case "google"
+
+                Dim url As String = "https://www.google.com/"
+                Process.Start(url)
+
+        End Select
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        Me.Close()
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+
+    Private Sub FacebookToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FacebookToolStripMenuItem.Click
+
+        Dim url As String = "https://www.facebook.com/sadik.fattah.7"
+        Process.Start(url)
+    End Sub
+
+    Private Sub PlusToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PlusToolStripMenuItem.Click
+        Dim url As String = "https://plus.google.com/110268382009035036954"
+        Process.Start(url)
+    End Sub
+
+    Private Sub YoutubeToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles YoutubeToolStripMenuItem.Click
+        Dim url As String = "https://www.youtube.com/channel/UCZr2VGmXdJ-yGLUZztCVFGw"
+        Process.Start(url)
+    End Sub
+End Class
